e14c5d1efa2a3b20627a8add297a344b450fe95f
nikhil0387 <nikhilsharma0387@gmail.com>
2025-12-28 21:41:10 +0530

    Update API URL in App_State component

--- Client/src/context/App_State.jsx
+++ Client/src/context/App_State.jsx
@@ -3,7 +3,7 @@ import { AppContext } from "./App_Context";
 import axios from "axios";
 
 const App_State = (props) => {
-  const url = " https://recipox.onrender.com";
+  const url = " https://recipox.onrender.com/api";
   const [token, setToken] = useState("");
   const [recipe, setrecipe] = useState([]);
   const [savedRecipe, setsavedRecipe] = useState([]);
